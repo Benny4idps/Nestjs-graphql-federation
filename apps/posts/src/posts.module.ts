7fa1b3053f137292cb14995dc22fb99e58783234
Benny4idps <benny@4idps.com>
2023-06-12 11:57:58 +0800

    Init project

---
+++ apps/posts/src/posts.module.ts
@@ -0,0 +1,22 @@
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { PostsResolver } from './posts.resolver';
+import { PostsService } from './posts.service';
+import { UserResolver } from './user.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: {
+        federation: 2,
+      },
+    }),
+  ],
+  providers: [PostsResolver, PostsService, UserResolver],
+})
+export class PostsModule {}
